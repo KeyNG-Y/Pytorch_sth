e8adffe93b5fd6903a59f2964a2a7b7b090ba43a
KeyNG-Y <91374799+KeyNG-Y@users.noreply.github.com>
2021-09-25 20:07:41 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+###通过链式法则可以把最后一层的误差输出到中间层的权值上得到中间层的梯度信息
+##x-->w1,b1-->y1-->w2,b2-->y2
+x=torch.tensor(1.)
+print(x)
+w1=torch.tensor(2.,requires_grad=True)
+b1=torch.tensor(1.)
+w2=torch.tensor(2.,requires_grad=True)
+b2=torch.tensor(1.)
+y1=x*w1+b1
+y2=y1*w2+b2
+dy2_dy1=torch.autograd.grad(y2,[y1],retain_graph=True)[0]
+dy1_dw_1=torch.autograd.grad(y1,[w1],retain_graph=True)[0]
+dy2_dw1=torch.autograd.grad(y2,[w1],retain_graph=True)[0]
+print(dy2_dy1*dy1_dw_1)
+print(dy2_dw1)
