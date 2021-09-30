e511c7dfb24371157e82c94061f36a59d3e8e5e5
KeyNG-Y <91374799+KeyNG-Y@users.noreply.github.com>
2021-09-30 18:58:33 +0800

    Add files via upload

---
+++ 8、交叉验证.py
@@ -0,0 +1,25 @@
+##Train Set+Val Set+Test Set
+
+import torch
+from torchvision import transforms, datasets
+
+batch_size = 200
+train_db = datasets.MNIST("../data", train=True, download=True,
+                          transform=transforms.Compose([
+                              transforms.ToTensor(),
+                              transforms.Normalize((0.1307,), (0.3081,))
+                          ]))
+train_loader = torch.utils.data.DataLoader(train_db, batch_size=batch_size, shuffle=True)
+
+test_db = datasets.MNIST("..\data", train=False, transform=transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+]))
+test_loader = torch.utils.data.DataLoader(test_db, batch_size=batch_size, shuffle=True)
+
+print("train:", len(train_db), "test:", len(test_db))
+train_db, val_db = torch.utils.data.random_split(train_db, [50000, 10000])#分割
+print("db_1:", len(train_db), "db_2:", len(val_db))
+train_loader = torch.utils.data.DataLoader(train_db, batch_size=batch_size, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_db,batch_size=batch_size, shuffle=True)
+print("train_loader:", len(train_loader), "val_loader:", len(val_loader),"test_loader",len(test_loader))
